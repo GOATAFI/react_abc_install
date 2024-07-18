3c4134ab2c8cca7749dd8f59162399e69ca0f0dd
GOATAFI <projectexecutiveofpm@gmail.com>
2024-07-18 10:55:11 +0600

    recipe and recipelist

--- src/Recipe.jsx
+++ src/Recipe.jsx
@@ -1,15 +1,40 @@
 import { recipes } from "./recipes";
+// export default function Recipe() {
+//   const yummyRecipes = recipes.map((r) => (
+//     <div key={r.id}>
+//       <h1>{r.name}</h1>
+//       <ul>
+//         {r.ingredients.map((i) => (
+//           <li key={i}>{i} </li>
+//         ))}
+//       </ul>
+//     </div>
+//   ));
+//   return <div>{yummyRecipes}</div>;
+// }
+export function RecipeList({ id, name, ingredients }) {
+  //   console.log(ingredients);
+  //   console.log(typeof ingredients);
+  return (
+    <div>
+      <h2>{id}</h2>
+      <h1>Name: {name}</h1>
+      <p>
+        Ingredients :
+        {ingredients.map((i) => (
+          <li key={i}>{i}</li>
+        ))}
+      </p>
+    </div>
+  );
+}
 
 export default function Recipe() {
-  const yummyRecipes = recipes.map((r) => (
-    <div key={r.id}>
-      <h1>{r.name}</h1>
-      <ul>
-        {r.ingredients.map((i) => (
-          <li key={i}>{i} </li>
-        ))}
-      </ul>
+  return (
+    <div>
+      {recipes.map((recipe) => (
+        <RecipeList key={recipe.id} {...recipe} />
+      ))}
     </div>
-  ));
-  return <div>{yummyRecipes}</div>;
+  );
 }
